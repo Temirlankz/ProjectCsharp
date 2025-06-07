1cbc00cdcdac36045d391fd6b6bd98302b7a888e
Temirlankz <altaytemirlan@mail.ru>
2025-06-08 02:13:03 +0300

    Added other file that will read files filling

--- Agent008/Program.cs
+++ Agent008/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Agent008
 {
@@ -6,8 +7,30 @@ namespace Agent008
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ASD");
+            Console.WriteLine("Asd");
+
+            Console.Write("Enter the path to the directory with .txt files: ");
+            string directoryPath = Console.ReadLine();
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine("Directory does not exist.");
+                return;
+            }
+
+            string[] files = Directory.GetFiles(directoryPath, "*.txt");
+
+            if (files.Length == 0)
+            {
+                Console.WriteLine("No .txt files found.");
+                return;
+            }
+
+            Console.WriteLine("\nText files found:");
+            foreach (string file in files)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
         }
     }
 }
-
