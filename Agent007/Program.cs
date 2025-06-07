5ee6486ea0ec9bb2905ee0cacad72a9409866ba5
Temirlankz <altaytemirlan@mail.ru>
2025-06-08 01:40:05 +0300

    Add project files.

---
+++ Agent007/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Agent007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asd");
+        }
+    }
+}
+
---
+++ ProjectC#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asd");
+        }
+    }
+}
+
