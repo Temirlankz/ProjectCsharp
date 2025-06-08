67d0cb6105d8629417891f090e12b259b1d5ace5
Temirlankz <altaytemirlan@mail.ru>
2025-06-08 14:30:13 +0300

    Added pipe to master

--- ProjectC#/Program.cs
+++ ProjectC#/Program.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
 
-namespace Chef
+namespace ProjectCSharp
 {
-    class Program
+    class Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ASD");
+            Console.WriteLine("Master process started");
+
+            Thread agent1Thread = new Thread(() => ListenToAgent("pipe_agent007"));
+            Thread agent2Thread = new Thread(() => ListenToAgent("pipe_agent008"));
+
+            agent1Thread.Start();
+            agent2Thread.Start();
+
+            agent1Thread.Join();
+            agent2Thread.Join();
+
+            Console.WriteLine("Master process finished");
+        }
+
+        static void ListenToAgent(string pipeName)
+        {
+            using (var pipe = new NamedPipeServerStream(pipeName, PipeDirection.In))
+            {
+                Console.WriteLine($"Connecting {pipeName}...");
+                pipe.WaitForConnection();
+                Console.WriteLine($"{pipeName} connected");
+
+                using (StreamReader reader = new StreamReader(pipe))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine($"[{pipeName}] {line}");
+                    }
+                }
+            }
         }
     }
 }
-
