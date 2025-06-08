4fab050e2b4e428f2151a885c407e859d79b2500
Temirlankz <altaytemirlan@mail.ru>
2025-06-08 15:18:17 +0300

    Added the counter for total and make that project start once

--- ProjectC#/Program.cs
+++ ProjectC#/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.IO.Pipes;
 using System.Threading;
@@ -7,12 +8,14 @@ namespace ProjectCSharp
 {
     class Project
     {
+        static ConcurrentDictionary<string, Dictionary<string, int>> mergedData = new();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Master process started");
+            Console.WriteLine("Master started");
 
-            Thread agent1Thread = new Thread(() => ListenToAgent("pipe_agent007"));
-            Thread agent2Thread = new Thread(() => ListenToAgent("pipe_agent008"));
+            Thread agent1Thread = new Thread(() => ListenToAgent("agent007"));
+            Thread agent2Thread = new Thread(() => ListenToAgent("agent008"));
 
             agent1Thread.Start();
             agent2Thread.Start();
@@ -20,26 +23,53 @@ namespace ProjectCSharp
             agent1Thread.Join();
             agent2Thread.Join();
 
-            Console.WriteLine("Master process finished");
+            Console.WriteLine("\n Final count result \n");
+            foreach (var fileEntry in mergedData)
+            {
+                foreach (var wordEntry in fileEntry.Value)
+                {
+                    Console.WriteLine($"{fileEntry.Key}:{wordEntry.Key}:{wordEntry.Value}");
+                }
+            }
+
+            Console.WriteLine("\nFinished.");
         }
 
         static void ListenToAgent(string pipeName)
         {
-            using (var pipe = new NamedPipeServerStream(pipeName, PipeDirection.In))
-            {
-                Console.WriteLine($"Connecting {pipeName}...");
-                pipe.WaitForConnection();
-                Console.WriteLine($"{pipeName} connected");
+            using var pipe = new NamedPipeServerStream(pipeName, PipeDirection.In);
+            Console.WriteLine($"Connecting {pipeName}");
+            pipe.WaitForConnection();
+            Console.WriteLine($"{pipeName} connected");
 
-                using (StreamReader reader = new StreamReader(pipe))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        Console.WriteLine($"[{pipeName}] {line}");
-                    }
-                }
+            using var reader = new StreamReader(pipe);
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine($"[{pipeName}] {line}");
+                ProcessLine(line);
             }
+        }
+
+        static void ProcessLine(string line)
+        {
+            var parts = line.Split(':');
+            if (parts.Length != 3) return;
+
+            string file = parts[0];
+            string word = parts[1];
+            if (!int.TryParse(parts[2], out int count)) return;
+
+            mergedData.AddOrUpdate(file,
+                _ => new Dictionary<string, int> { [word] = count },
+                (_, dict) =>
+                {
+                    if (dict.ContainsKey(word))
+                        dict[word] += count;
+                    else
+                        dict[word] = count;
+                    return dict;
+                });
         }
     }
 }
